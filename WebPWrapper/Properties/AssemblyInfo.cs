407acfcb576469688dce67ddb4d792ea89324351
René Rhéaume <rene.rheaume@gmail.com>
2021-09-10 13:49:48 -0400

    Reorganized Assembly Information files for readability and consistency.

--- WebPWrapper/Properties/AssemblyInfo.cs
+++ WebPWrapper/Properties/AssemblyInfo.cs
@@ -1,27 +1,37 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
+#region General Information
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#endregion
 [assembly: AssemblyTitle("WebP Wrapper for .NET")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("WebP Wrapper for .NET")]
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2021 René Rhéaume. ")]
+[assembly: AssemblyProduct("WebP-Wrapper")]
+[assembly: AssemblyCopyright("Copyright © 2012-2019 Jose M. Piñeiro, 2021 René Rhéaume")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#region Interoperability Attributes
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The GUID is for the ID of the typelib if this project is exposed to COM
+#endregion
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
 [assembly: Guid("daf13bd3-52b9-4fcb-89e9-49dedf5e0f20")]
 
+#region Version Information
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +42,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#endregion
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
