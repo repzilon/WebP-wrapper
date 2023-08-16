d8c35d14c2e2a2f6a615510335cc638fe0129d38
repzilon <6383002+repzilon@users.noreply.github.com>
2023-08-16 13:33:01 -0400

    Relicense WebP42 under AGPL 3
    I am the original author of this utility and I plan to implement an algorithm that needs a special permission from its author.

--- WebP42/Program.cs
+++ WebP42/Program.cs
@@ -1,4 +1,25 @@
-﻿#define LossyExperiment
+﻿//
+//  Program.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+//  Copyright (c) 2022-2023 René Rhéaume
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#define LossyExperiment
 #define NearLosslessExperiment
 using System;
 using System.Collections.Generic;
