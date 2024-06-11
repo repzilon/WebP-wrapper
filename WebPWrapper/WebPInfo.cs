a6d27007290ba79f10a719225794b34e8cc10a5e
repzilon <6383002+repzilon@users.noreply.github.com>
2024-06-10 21:41:15 -0400

    Force unchecked bounds arithmetic in GetHashCode

--- WebPWrapper/WebPInfo.cs
+++ WebPWrapper/WebPInfo.cs
@@ -21,7 +21,7 @@ namespace WebPWrapper
 			return obj is WebPInfo && this.Equals((WebPInfo)obj);
 		}
 
-		public bool Equals(WebPInfo other)
+		public readonly bool Equals(WebPInfo other)
 		{
 			return Width == other.Width &&
 				   Height == other.Height &&
@@ -32,22 +32,24 @@ namespace WebPWrapper
 
 		public override int GetHashCode()
 		{
+			unchecked {
 #pragma warning disable U2U1000 // Local variable can be inlined or declared constant
 #pragma warning disable RCS1118 // Mark local variable as constant.
-			/* const */
-			int hashKey = -1521134295; // being a variable makes IL smaller
+				// ReSharper disable once ConvertToConstant.Local
+				/* const */ int hashKey = -1521134295; // being a variable makes IL smaller
 #pragma warning restore RCS1118 // Mark local variable as constant.
 #pragma warning restore U2U1000 // Local variable can be inlined or declared constant
-			int hashCode = 1439674596;
-			// This is a non-issue because this is a structure, not a class.
+				int hashCode = 1439674596;
+				// This is a non-issue because this is a structure, not a class.
 #pragma warning disable RECS0025 // Non read-only field referenced in 'GetHashCode()'
-			hashCode = (hashCode * hashKey) + Width.GetHashCode();
-			hashCode = (hashCode * hashKey) + Height.GetHashCode();
-			hashCode = (hashCode * hashKey) + HasAlpha.GetHashCode();
-			hashCode = (hashCode * hashKey) + IsAnimated.GetHashCode();
-			hashCode = (hashCode * hashKey) + EqualityComparer<string>.Default.GetHashCode(Format);
+				hashCode = (hashCode * hashKey) + Width;
+				hashCode = (hashCode * hashKey) + Height;
+				hashCode = (hashCode * hashKey) + HasAlpha.GetHashCode();
+				hashCode = (hashCode * hashKey) + IsAnimated.GetHashCode();
+				hashCode = (hashCode * hashKey) + EqualityComparer<string>.Default.GetHashCode(Format);
 #pragma warning restore RECS0025 // Non read-only field referenced in 'GetHashCode()'
-			return hashCode;
+				return hashCode;
+			}
 		}
 
 		public static bool operator ==(WebPInfo info1, WebPInfo info2)
